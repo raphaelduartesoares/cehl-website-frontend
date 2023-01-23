8593b31711db3d59c58cc18ae77bad7165101017
Raphael Soares <raphael.duartesoares@gmail.com>
2023-01-22 22:24:21 -0300

    feat: includes success toast after copy qrcode or cnpj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxNotifierModule } from "ngx-notifier";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FooterComponent } from "./views/components/footer/footer.component";
+import { DoacaoComponent } from "./views/pages/doacao/doacao.component";
 import { LinksComponent } from "./views/pages/links/links.component";
-import { DoacaoComponent } from './views/pages/doacao/doacao.component';
-import { FooterComponent } from './views/components/footer/footer.component';
 
 @NgModule({
     declarations: [AppComponent, LinksComponent, DoacaoComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NgxNotifierModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/views/pages/doacao/doacao.component.ts
+++ src/app/views/pages/doacao/doacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgxNotifierService } from "ngx-notifier";
 
 @Component({
     templateUrl: "./doacao.component.html",
@@ -9,12 +10,16 @@ export class DoacaoComponent {
         "00020126360014BR.GOV.BCB.PIX0114111440370001005204000053039865802BR5904CEHL6008Jaboatao62190515DoacaoPIXQRCode630405DD";
     private cnpj = "11144037000100";
 
+    constructor(private ngxNotifierService: NgxNotifierService) {}
+
     onCopyQRCode(): void {
         this.copyTextToClipboard(this.qrCode);
+        this.ngxNotifierService.createToast("Código QR copiado!", "success");
     }
 
     onCopyCNPJ(): void {
         this.copyTextToClipboard(this.cnpj);
+        this.ngxNotifierService.createToast("CNPJ copiado!", "success");
     }
 
     private copyTextToClipboard(text: string): void {
