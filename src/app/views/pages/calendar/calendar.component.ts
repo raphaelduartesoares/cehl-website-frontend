09a375888fe1cb0fa3a1d151107d32e22b9c3a4e
Raphael Soares <raphael.duartesoares@gmail.com>
2023-02-06 16:47:36 -0300

    feat: includes switch calendar view when in small screens

--- src/app/views/pages/calendar/calendar.component.ts
+++ src/app/views/pages/calendar/calendar.component.ts
@@ -1,16 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 
 @Component({
     templateUrl: "./calendar.component.html",
     styleUrls: ["./calendar.component.scss"],
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
     urlCalendarEventsApple =
         "webcal://calendar.google.com/calendar/ical/ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com/public/basic.ics";
     urlCalendarEventsGoogle =
         "https://calendar.google.com/calendar/render?cid=ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com";
 
     urlCalendarEvents = "";
+    innerWidth: any;
+
+    SMALL_SCREEN_BREAKPOINT = 576;
 
     constructor() {
         const userAgent = navigator.userAgent;
@@ -19,5 +22,9 @@ export class CalendarComponent {
         } else {
             this.urlCalendarEvents = this.urlCalendarEventsGoogle;
         }
+    }
+
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
     }
 }
