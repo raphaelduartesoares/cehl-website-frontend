077b6d6c682d2449ac2a1705e64bffd4cd328da4
Raphael Soares <raphael.duartesoares@gmail.com>
2023-02-05 22:53:15 -0300

    feat: includes add eventos to personal calendar button

--- src/app/views/pages/calendar/calendar.component.ts
+++ src/app/views/pages/calendar/calendar.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+    templateUrl: "./calendar.component.html",
+    styleUrls: ["./calendar.component.scss"],
 })
 export class CalendarComponent {
+    urlCalendarEventsApple =
+        "webcal://calendar.google.com/calendar/ical/ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com/public/basic.ics";
+    urlCalendarEventsGoogle =
+        "https://calendar.google.com/calendar/render?cid=ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com";
 
+    urlCalendarEvents = "";
+
+    constructor() {
+        const userAgent = navigator.userAgent;
+        if (userAgent.indexOf("iPhone") !== -1) {
+            this.urlCalendarEvents = this.urlCalendarEventsApple;
+        } else {
+            this.urlCalendarEvents = this.urlCalendarEventsGoogle;
+        }
+    }
 }
--- src/app/views/pages/links/links.component.ts
+++ src/app/views/pages/links/links.component.ts
@@ -4,20 +4,4 @@ import { Component } from "@angular/core";
     templateUrl: "./links.component.html",
     styleUrls: ["./links.component.scss"],
 })
-export class LinksComponent {
-    urlCalendarEventsApple =
-        "webcal://calendar.google.com/calendar/ical/ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com/public/basic.ics";
-    urlCalendarEventsGoogle =
-        "https://calendar.google.com/calendar/render?cid=ffe2f7f4cd54afaeb8863335953386a081d430d13b595bd79150504ed831f2a1@group.calendar.google.com";
-
-    urlCalendarEvents = "";
-
-    constructor() {
-        const userAgent = navigator.userAgent;
-        if (userAgent.indexOf("iPhone") !== -1) {
-            this.urlCalendarEvents = this.urlCalendarEventsApple;
-        } else {
-            this.urlCalendarEvents = this.urlCalendarEventsGoogle;
-        }
-    }
-}
+export class LinksComponent {}
